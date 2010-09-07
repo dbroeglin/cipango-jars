c40ddfc3b00abc2c45d56f90043cd7532a266ff5
Dominique Broeglin <dominique.broeglin@gmail.com>
2010-09-08 01:10:46 +0200

    WIP: First functional version, still needs some work before publishing

---
+++ lib/cipango-jars.rb
@@ -0,0 +1,28 @@
+module CipangoJars
+  PATH = File.expand_path(File.dirname(__FILE__))
+
+  class << self  
+    def all_jar_names
+      @all_jar_names ||= []
+    end
+    
+    def all_jar_paths
+      @all_jar_paths ||= []
+    end
+    
+    {
+      'cipango' => '1.0', 
+      'sip-api' => '1.1', 
+      'jetty' => '6.1.24', 
+      'jetty-util' => '6.1.24', 
+      'servlet-api' => '2.5-20081211'
+    }.each_pair do |name, version|
+      uname = name.gsub(/-/, '_')
+      jar_name = "#{name}-#{version}.jar"
+      jar_path = "#{PATH}/#{jar_name}"
+      define_method("#{uname}_jar_path") { jar_path }
+      CipangoJars.all_jar_names << jar_name
+      CipangoJars.all_jar_paths << jar_path
+    end
+  end
+end
